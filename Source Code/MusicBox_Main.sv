b7623056f9f437af383606a8e1f9c59a3ba85090
Graham Mueller <GrahamMueller1992@gmail.com>
2020-01-22 20:32:56 -0800

    Created more formal pin naming for the DE10 Lite board

--- Source Code/MusicBox_Main.sv
+++ Source Code/MusicBox_Main.sv
@@ -1,19 +1,44 @@
 module MusicBox_Main(
 	max10Board_Button0,
 	max10Board_Button1,
-	
 	max10board_switches,
-	max10Board_LEDs
+	
+	max10Board_50MhzClock,
+	max10Board_LEDSegments,
+	max10Board_LEDs,
+	max10Board_SDRAM_Clock,
+	max10Board_SDRAM_ClockEnable,
+	max10Board_SDRAM_Address,
+	max10Board_SDRAM_BankAddress,
+	max10Board_SDRAM_Data,
+	max10Board_SDRAM_DataMask0,
+	max10Board_SDRAM_DataMask1,
+	max10Board_SDRAM_ChipSelect_n,
+	max10Board_SDRAM_WriteEnable_n,
+	max10Board_SDRAM_ColumnAddressStrobe_n,
+	max10Board_SDRAM_RowAddressStrobe_n
 );
-
-
+	input wire	max10Board_50MhzClock;
+	output wire	[5:0][6:0]	max10Board_LEDSegments;
+	output reg [9:0] max10Board_LEDs; //The LED lights
+	///////// SDRAM /////////
+	output wire max10Board_SDRAM_Clock;
+	output wire max10Board_SDRAM_ClockEnable;
+	output wire [12: 0]   max10Board_SDRAM_Address;
+	output wire [ 1: 0]   max10Board_SDRAM_BankAddress;
+	inout wire [15: 0]   max10Board_SDRAM_Data;
+	input wire [9:0] max10board_switches;
+	
+	output wire max10Board_SDRAM_DataMask0;
+	output wire max10Board_SDRAM_DataMask1;
+	output wire max10Board_SDRAM_ChipSelect_n; //active low
+	output wire max10Board_SDRAM_WriteEnable_n; //active low
+	output wire max10Board_SDRAM_ColumnAddressStrobe_n; //active low
+	output wire max10Board_SDRAM_RowAddressStrobe_n; //active low
+	/////////////////////////////////////////////////////////
 	//-- 
 	input wire	max10Board_Button0 ;
 	input wire	max10Board_Button1; //Controls reset functionality
-	
-	input wire [9:0] max10board_switches;
-	
-	output reg [9:0] max10Board_LEDs; //The LED lights
 	
 	
 endmodule
