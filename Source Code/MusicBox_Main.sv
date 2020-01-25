68c24032afb8e4291c8d98ccca4387a3e48695a0
Graham Mueller <GrahamMueller1992@gmail.com>
2020-01-25 11:01:52 -0800

    Updated IO settings

--- Source Code/MusicBox_Main.sv
+++ Source Code/MusicBox_Main.sv
@@ -1,6 +1,5 @@
 module MusicBox_Main(
-	max10Board_Button0,
-	max10Board_Button1,
+	max10Board_Buttons,
 	max10board_switches,
 	
 	max10Board_50MhzClock,
@@ -18,12 +17,27 @@ module MusicBox_Main(
 	max10Board_SDRAM_ColumnAddressStrobe_n,
 	max10Board_SDRAM_RowAddressStrobe_n,
 	
-	max10Board_ExternalPins
+	//max10Board_GPIO_Input //
+	//max10Board_GPIO_Output //
+	
+	max10Board_GPIO_Input_MusicKeys, //Array
+	max10Board_GPIO_Input_PlaySong1,
+	max10Board_GPIO_Input_PlaySong0,
+	max10Board_GPIO_Input_MakeRecording,
+	max10Board_GPIO_Input_PlayRecording
 );
 	input wire	max10Board_50MhzClock;
-	output wire	[5:0][6:0]	max10Board_LEDSegments;
-	output reg [9:0] max10Board_LEDs; //The LED lights
-	input wire [9:0] max10Board_ExternalPins;
+	output wire	[5:0][6:0]	max10Board_LEDSegments;//The DE-10 Board LED Segments
+	output reg [9:0] max10Board_LEDs; //The DE-10 Board LED lights
+	
+	//input wire [35:11] max10Board_GPIO; 
+	//output wire [10:0] max10Board_GPIO; 
+	///////// UI GPIO ///////
+	input wire [5:0] max10Board_GPIO_Input_MusicKeys; //Array
+	input wire max10Board_GPIO_Input_PlaySong1;
+	input wire max10Board_GPIO_Input_PlaySong0;
+	input wire max10Board_GPIO_Input_MakeRecording;
+	input wire max10Board_GPIO_Input_PlayRecording;
 	///////// SDRAM /////////
 	output wire max10Board_SDRAM_Clock;
 	output wire max10Board_SDRAM_ClockEnable;
@@ -40,8 +54,17 @@ module MusicBox_Main(
 	output wire max10Board_SDRAM_RowAddressStrobe_n; //active low
 	/////////////////////////////////////////////////////////
 	//-- 
-	input wire	max10Board_Button0 ;
-	input wire	max10Board_Button1; //Controls reset functionality
+	input wire	[1: 0] max10Board_Buttons ;
 	
+	
+	
+	/*
+	Do simple button UI
+	Do simple button UI on GPIO
+		test voltage
+		get active low
+		active high
+		
+	*/
 	
 endmodule
